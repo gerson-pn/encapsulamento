d9b668695dbcc69dd5c18c6ceae7c086914afd2e
Penha <Penha@DESKTOP-T7547TI>
2021-08-12 11:51:09 -0300

    inclusão do método set

--- encapsulamento/src/com/gpn/App.java
+++ encapsulamento/src/com/gpn/App.java
@@ -3,9 +3,11 @@ package com.gpn;
 public class App {
 	public static void main(String[] args) {
 
-		Empresa ep = new Empresa("99999999999");
+		Empresa ep = new Empresa("99999999999","ABC LTDA","Seara");
+		
+		ep.setRazaoSocial("gpn ltda");
 
-		System.out.println("Qual é o cnpj: " + ep.getCnpj());
+		System.out.println("Qual é o cnpj: " + ep.getRazaoSocial());
 
 	}
 }
--- encapsulamento/src/com/gpn/Empresa.java
+++ encapsulamento/src/com/gpn/Empresa.java
@@ -2,13 +2,33 @@ package com.gpn;
 
 public class Empresa {
 	private String cnpj;
+	private String razaoSocial;
+	private String nomeFantasia;
 
-	public Empresa(String cnpj) {
+	public Empresa(String cnpj, String razaoSocial, String nomeFantasia) {
 		this.cnpj = cnpj;
+		this.razaoSocial = razaoSocial;
+		this.nomeFantasia = nomeFantasia;
 	}
-	
+
 	public String getCnpj() {
 		return this.cnpj;
 	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial.toUpperCase();
+	}
 	
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
 }
