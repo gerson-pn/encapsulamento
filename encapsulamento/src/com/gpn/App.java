6ea4ee429a2d956be1cc50b8fdfc52ba36ce98d2
Penha <Penha@DESKTOP-T7547TI>
2021-08-12 11:30:32 -0300

    inclusão do projeto de encapsulamento

---
+++ encapsulamento/src/com/gpn/App.java
@@ -0,0 +1,9 @@
+package com.gpn;
+
+public class App {
+	public static void main(String[] args) {
+
+		Empresa ep = new Empresa("99999999999");
+
+	}
+}
---
+++ encapsulamento/src/com/gpn/Empresa.java
@@ -0,0 +1,10 @@
+package com.gpn;
+
+public class Empresa {
+	private String cnpj;
+
+	public Empresa(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+}
